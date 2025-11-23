b6ac232d313686621ad1bc617a91e4c22a759ba7
root <root@ip-172-31-89-133.ec2.internal>
2025-11-23 11:49:40 +0000

    Intial Commit

--- Folderscript.sh
+++ Folderscript.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# create a folder
-
+# Create a folder
 mkdir Practice_folder
 
-#create three files
+# Move into the folder
 cd Practice_folder
+
+# Create three files
 touch prac1 prac2 prac3
 
+# Delete one file (example: delete prac2)
+rm prac2
+
+echo "Directory created, files created, and prac2 deleted successfully."
 
